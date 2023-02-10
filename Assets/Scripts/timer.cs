29bd39e596ea0f924bdfbb465e673e04e08042ae
Arianne Gleiser <agleiser@usc.edu>
2023-02-10 13:00:43 -0800

    fixed a few errors

--- Assets/Scripts/timer.cs
+++ Assets/Scripts/timer.cs
@@ -10,7 +10,7 @@ public class timer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        disvar.text = "";
     }
 
     // Update is called once per frame
@@ -21,7 +21,7 @@ public class timer : MonoBehaviour
             countdown -= Time.deltaTime;
         }
         double b = System.Math.Round(countdown, 2);
-        disvar.text = b.ToString();
+        disvar.text = "Time Remaining: " + b.ToString();
         if (countdown < 0)
         {
             disvar.text = "Game Over!";
