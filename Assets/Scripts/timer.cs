e6051a1c60f8184553313613bac793648fbd83c3
Arianne Gleiser <agleiser@usc.edu>
2023-02-10 12:38:53 -0800

    added the timer script

---
+++ Assets/Scripts/timer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class timer : MonoBehaviour
+{
+    float countdown = 60f;
+    public TMP_Text disvar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (countdown > 0)
+        {
+            countdown -= Time.deltaTime;
+        }
+        double b = System.Math.Round(countdown, 2);
+        disvar.text = b.ToString();
+        if (countdown < 0)
+        {
+            disvar.text = "Game Over!";
+        }
+    }
+}
