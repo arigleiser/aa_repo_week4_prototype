5bd65ee6185270dcec7a7e7154e7c65b7f19aec9
Arianne Gleiser <agleiser@usc.edu>
2023-02-12 12:26:39 -0800

    sounds

--- Assets/Scripts/obstacles.cs
+++ Assets/Scripts/obstacles.cs
@@ -10,6 +10,7 @@ public class obstacles : MonoBehaviour
     GameObject ufoClone;
     public loseIfTouchApple touchApple;
     public gameManagerScript gm;
+    public AudioSource appleFallingSound;
 
     void Start()
     {
@@ -28,6 +29,7 @@ public class obstacles : MonoBehaviour
         {
             float spawnY = Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height)).y + 3.0f;
             ufoClone = Instantiate(ufo, new Vector3(Random.Range(-7, 7), spawnY, 0f), transform.rotation) as GameObject;
+            appleFallingSound.Play();
             Timer = 4f;
         }
     }
